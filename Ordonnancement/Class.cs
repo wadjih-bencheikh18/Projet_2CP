9fd2393a8d546cbaf1aead735fd5e1faf0adc13a
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2021-04-20 15:36:22 +0100

    done 2

--- Ordonnancement/Class.cs
+++ Ordonnancement/Class.cs
@@ -62,24 +62,4 @@ namespace Ordonnancement
         public abstract void CalculService();
 
     }
-
-    class Programme
-    {
-        static void Main(string[] args)
-        {
-            Processus Pro;
-            PAPS prgm = new();
-            Random r1, r2, r3, r4;
-            for (int i = 0; i < 20; i++)
-            {
-                r1 = new Random();
-                r2 = new Random();
-                r3 = new Random();
-                r4 = new Random();
-                Pro = new(r1.Next(0, 100), r2.Next(0, 100), r3.Next(1, 100), r4.Next(0, 100));
-                prgm.Push(Pro);
-            }
-            prgm.Faire();
-        }
-    }
 }
