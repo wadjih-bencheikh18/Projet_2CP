a907572a1c69f2f692a4f29ec53547c9a56e241a
moonayyur <jm_kias@esi.dz>
2021-04-30 19:11:22 +0100

    added some comments and fixed some instructions fi PAPS PCA Class

--- Ordonnancement/PAPS.cs
+++ Ordonnancement/PAPS.cs
@@ -10,29 +10,28 @@ namespace Ordonnancement
         {
             SortListeProcessus(); //tri des processus par ordre d'arrivé
             int temps = 0, indice = 0;
-            while (indice < listeProcessus.Count || listeExecution.Count != 0) //s'il existe des processus non executés et le temps < le temps de fin
+            while (indice < listeProcessus.Count || listeExecution.Count != 0) //s'il existe des processus non executés
             {
                 indice = AjouterTous(temps, indice); //remplir listeExecution
                 temps++; //incrementer le temps réel
                 if (listeExecution.Count != 0) //s'il y a des processus à executer
                 {
                     listeExecution[0].tempsRestant--; //l'execution du 1er processus de listeExecution commence
-                    AfficheLigne(temps - 1, listeExecution[0].id);
+                    AfficheLigne(temps - 1, listeExecution[0].id); //affiche le temps actuel et l'ID du processus entrain d'être executé
                     if (listeExecution[0].tempsRestant == 0) //si l'execution du premier processus de listeExecution est terminée
                     {
                         listeExecution[0].tempsFin = temps; //temps de fin d'execution = au temps actuel
                         listeExecution[0].tempsService = temps - listeExecution[0].tempsArriv; //temps de service = temps de fin d'execution - temps d'arrivé
-                        listeExecution[0].tempsAtt = listeExecution[0].tempsService - listeExecution[0].duree;
+                        listeExecution[0].tempsAtt = listeExecution[0].tempsService - listeExecution[0].duree; //temps d'attente = temps de service - durée d'execution
                         listeExecution.RemoveAt(0); //supprimer le premier processus executé
                     }
                 }
-                else AfficheLigne(temps-1);
+                else AfficheLigne(temps-1); //affiche le temps actuel et le mot "repos" ie le processeur n'execute aucun processus
             }
             return temps;
         }
 
-
-        // des algos pour utiliser dans MultiNiveaux
+        // à utiliser dans MultiNiveaux
         public void InitPAPS(List<Processus> listeProcessus, List<Processus> listeExecution)
         {
             this.listeProcessus = listeProcessus;
@@ -44,17 +43,17 @@ namespace Ordonnancement
             int temps = tempsDebut;
             while (listeExecution.Count != 0 && temps < tempsFin) //s'il existe des processus non executés et le temps < le temps de fin
             {
-                niveaux[indiceNiveau].indice[0] = AjouterTous(temps,niveaux[indiceNiveau].indice[0],niveaux,listeGeneral,indiceNiveau); //remplir listeExecution
+                niveaux[indiceNiveau].indice[0] = AjouterTous(temps,niveaux[indiceNiveau].indice[0],niveaux,listeGeneral,indiceNiveau); //
                 temps++; //incrementer le temps réel
                 if (listeExecution.Count != 0) //s'il y a des processus à executer
                 {
                     listeExecution[0].tempsRestant--; //l'execution du 1er processus de listeExecution commence
-                    AfficheLigne(temps - 1, listeExecution[0].id);
+                    AfficheLigne(temps - 1, listeExecution[0].id); //affiche le temps actuel et l'ID du processus entrain d'être executé
                     if (listeExecution[0].tempsRestant == 0) //si l'execution du premier processus de listeExecution est terminée
                     {
                         listeExecution[0].tempsFin = temps; //temps de fin d'execution = au temps actuel
                         listeExecution[0].tempsService = temps - listeExecution[0].tempsArriv; //temps de service = temps de fin d'execution - temps d'arrivé
-                        listeExecution[0].tempsAtt = listeExecution[0].tempsService - listeExecution[0].duree;
+                        listeExecution[0].tempsAtt = listeExecution[0].tempsService - listeExecution[0].duree;  //temps d'attente = temps de service - durée d'execution
                         listeExecution.RemoveAt(0); //supprimer le premier processus executé
                     }
                 }
--- Ordonnancement/PCA.cs
+++ Ordonnancement/PCA.cs
@@ -7,39 +7,38 @@ namespace Ordonnancement
         public PCA() { }
         public int Executer()
         {
-            SortListeProcessus(); //tri par ordre d'arrivé
+            SortListeProcessus(); //tri des processus par ordre d'arrivé
             int temps = 0, indice = 0;
-            bool sort = true;
-            while ((indice < listeProcessus.Count || listeExecution.Count != 0))
+            bool sort = true; //est à vrai si un tri par durée est necessaire
+            while ((indice < listeProcessus.Count || listeExecution.Count != 0)) //s'il existe des processus non executés
             {
                 if (listeExecution.Count == 0) sort = true; //les premiers processus arrivés => on fait le tri pour avoir la plus courte durée
-                indice = AjouterTous(temps, indice);  //remplir list Execution
-                if (sort == true && listeExecution.Count != 0)
+                indice = AjouterTous(temps, indice);  //remplir listeExecution
+                if (sort == true && listeExecution.Count != 0) //si un tri est necessaire et il y a des processus à executer
                 {
-                    listeExecution.Sort(delegate (Processus x, Processus y) { return y.duree.CompareTo(x.duree); }); //tri par duree
-                    sort = false;
+                    listeExecution.Sort(delegate (Processus x, Processus y) { return y.duree.CompareTo(x.duree); }); //tri des processus de listeExecution par durée
+                    sort = false; //le tri par durée n'est plus necessaire (déja fait)
                 }
-                temps++;
+                temps++; //incrementer le temps réel
                 if (listeExecution.Count != 0) //il y a des processus à exécuter
                 {
-                    listeExecution[0].tempsRestant--; //le processus est entrain de s'exécuter => décrémenter le tempsRestant
-                    AfficheLigne(temps - 1, listeExecution[0].id);
-                    if (listeExecution[0].tempsRestant == 0) // fin d'exécution du processus 
+                    listeExecution[0].tempsRestant--; //execution du 1er processus de listeExecution => décrémenter le tempsRestant
+                    AfficheLigne(temps - 1, listeExecution[0].id); //affiche le temps actuel et l'ID du processus entrain d'être executé
+                    if (listeExecution[0].tempsRestant == 0) //si l'execution du premier processus de listeExecution est terminée
                     {
-                        listeExecution[0].tempsFin = temps;
-                        listeExecution[0].tempsService = temps - listeExecution[0].tempsArriv;
-                        listeExecution[0].tempsAtt = listeExecution[0].tempsService - listeExecution[0].duree;
-                        listeExecution.RemoveAt(0);
-                        // on tri les processus restants
-                        if (listeExecution.Count != 0) sort = true;
+                        listeExecution[0].tempsFin = temps; //temps de fin d'execution = au temps actuel
+                        listeExecution[0].tempsService = temps - listeExecution[0].tempsArriv; //temps de service = temps de fin d'execution - temps d'arrivé
+                        listeExecution[0].tempsAtt = listeExecution[0].tempsService - listeExecution[0].duree; //temps d'attente = temps de service - durée d'execution
+                        listeExecution.RemoveAt(0); //supprimer le premier processus executé
+                        if (listeExecution.Count != 0) sort = true; //tri des processus restants dans listeExecution
                     }
                 }
-                else AfficheLigne(temps - 1);
+                else AfficheLigne(temps - 1); //affiche le temps actuel et le mot "repos" ie le processeur n'execute aucun processus
             }
             return temps;
         }
 
-        // des algos pour utiliser dans MultiNiveaux
+        // à utiliser dans MultiNiveaux
         public void InitPCA(List<Processus> listeProcessus, List<Processus> listeExecution)
         {
             this.listeProcessus = listeProcessus;
@@ -47,29 +46,29 @@ namespace Ordonnancement
         }
         public int Executer(int tempsDebut, int tempsFin, Niveau[] niveaux, int indiceNiveau, List<ProcessusNiveau> listeGeneral)
         {
-            SortListeProcessus(); //tri par ordre d'arrivé
+            SortListeProcessus(); //tri des processus par ordre d'arrivé
             int temps = tempsDebut;
-            while (listeExecution.Count != 0 && temps < tempsFin)
+            while (listeExecution.Count != 0 && temps < tempsFin) //s'il existe des processus non executés et le temps < le temps de fin
             {
-                if (listeExecution[0]==listeProcessus[0] && niveaux[indiceNiveau].indice[2]==0) niveaux[indiceNiveau].indice[1] = 1; //les premiers processus arrivés => on fait le tri pour avoir la plus courte durée
+                if (listeExecution[0]==listeProcessus[0] && niveaux[indiceNiveau].indice[2]==0) niveaux[indiceNiveau].indice[1] = 1;
                 niveaux[indiceNiveau].indice[2] = 1;
                 niveaux[indiceNiveau].indice[0] = AjouterTous(temps, niveaux[indiceNiveau].indice[0], niveaux, listeGeneral,indiceNiveau);
                 if (niveaux[indiceNiveau].indice[1] == 1 && listeExecution.Count != 0)
                 {
-                    listeExecution.Sort(delegate (Processus x, Processus y) { return y.duree.CompareTo(x.duree); }); //tri par duree
+                    listeExecution.Sort(delegate (Processus x, Processus y) { return y.duree.CompareTo(x.duree); }); //tri des processus par durée
                     niveaux[indiceNiveau].indice[1] = 0;
                 }
-                temps++;
+                temps++; //incrementer le temps réel
                 if (listeExecution.Count != 0) //il y a des processus à exécuter
                 {
                     listeExecution[0].tempsRestant--; //le processus est entrain de s'exécuter => décrémenter le tempsRestant
-                    AfficheLigne(temps - 1, listeExecution[0].id);
+                    AfficheLigne(temps - 1, listeExecution[0].id); //affiche le temps actuel et l'ID du processus entrain d'être executé
                     if (listeExecution[0].tempsRestant == 0) // fin d'exécution du processus 
                     {
-                        listeExecution[0].tempsFin = temps;
-                        listeExecution[0].tempsService = temps - listeExecution[0].tempsArriv;
-                        listeExecution[0].tempsAtt = listeExecution[0].tempsService - listeExecution[0].duree;
-                        listeExecution.RemoveAt(0);
+                        listeExecution[0].tempsFin = temps; //temps de fin d'execution = au temps actuel
+                        listeExecution[0].tempsService = temps - listeExecution[0].tempsArriv; //temps de service = temps de fin d'execution - temps d'arrivé
+                        listeExecution[0].tempsAtt = listeExecution[0].tempsService - listeExecution[0].duree;  //temps d'attente = temps de service - durée d'execution
+                        listeExecution.RemoveAt(0); //supprimer le premier processus executé
                         // on tri les processus restants
                         if (listeExecution.Count != 0) niveaux[indiceNiveau].indice[1] = 1;
                     }
