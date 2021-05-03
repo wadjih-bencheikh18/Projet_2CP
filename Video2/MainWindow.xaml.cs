0d17f129c40f179bc588ef7489549255f3847c14
aymen-nyte <ja_bendakir@esi.dz>
2021-05-04 00:45:27 +0100

    Improvement to wpf initialisation page

--- Video2/MainWindow.xaml.cs
+++ Video2/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.ComponentModel;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -24,20 +25,70 @@ namespace Video2
         {
             InitializeComponent();
         }
+
         public class Processus
         {
             public string id{ get; set; }
             public string duree { get; set; }
             public string tempsArriv { get; set; }
-            public string tempsFin { get; set; }
-            public string tempsService { get; set; }
             public string Priorite { get; set; }
-            
+        }
+        
+        private void EnterButton_Click(object sender, RoutedEventArgs e)
+        {
+            bool random = (bool) this.RandomizeCheckBox.IsChecked;
+            int NbProcessus;
+            bool correct = int.TryParse(this.ProcessusNumTextBox.Text, out NbProcessus);
+            if (correct && NbProcessus > 0)
+            {
+                this.MainStackPanel.Children.RemoveRange(0, this.MainStackPanel.Children.Count - 1);  // vider la fenétre
+            }
+            else
+            {
+                if (correct)
+                {
+                    MessageBox.Show("Nombre des processus incorrect");
+                }
+                else
+                {
+                    MessageBox.Show("Input invalid");
+                }
+                this.ProcessusNumTextBox.Text = "";
+                return;
+            }
+            Random r = new Random();
+            for (int i = 0; i < NbProcessus; i++)
+            {
+                Processus pro = new Processus();
+                pro.id = (i + 1).ToString();
+                pro.duree = (random ? r.Next(1, 10) : 1).ToString();
+                pro.tempsArriv = (random ? r.Next(1, 10) : 0).ToString();
+                pro.Priorite = (random ? r.Next(1, 10) : 1).ToString();
+                this.InitTab.Items.Add(pro);
+            }
         }
 
-        private void ButtonClick(object sender, RoutedEventArgs e)
+        private void MyComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            if (this.MyComboBox.SelectedItem.ToString().EndsWith("Round Robin"))
+            {
+                this.NumberEntry.Visibility = Visibility.Visible;
+                this.MyTextBlock.Text = "Quantum:";
+            }
+            else if (this.MyComboBox.SelectedItem.ToString().EndsWith("Multi Niveaux"))
+            {
+                this.NumberEntry.Visibility = Visibility.Visible;
+                this.MyTextBlock.Text = "Nombre des Niveaux:";
+            }
+            else if (this.NumberEntry != null)
+            {
+                this.NumberEntry.Visibility = Visibility.Hidden;
+            }
+        }
 
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            MyComboBox_SelectionChanged(this.MyComboBox, null);
         }
     }
 }
