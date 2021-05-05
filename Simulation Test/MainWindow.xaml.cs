bbedf374eca0cf801cd90a9956dc9781030d1e2b
aymen-nyte <ja_bendakir@esi.dz>
2021-05-05 14:35:57 +0100

    visibility work

--- Simulation Test/MainWindow.xaml.cs
+++ Simulation Test/MainWindow.xaml.cs
@@ -24,6 +24,12 @@ namespace Simulation_Test
             idTB.Text = id.ToString();
             id++;
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            AlgoComboBox_SelectionChanged(this.AlgoComboBox, null);
+        }
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             bool Valid = true;
@@ -71,5 +77,71 @@ namespace Simulation_Test
             ResultatFinal resultatFinal = new ResultatFinal(P);
             resultatFinal.Show();
         }
+
+        private void AlgoComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (this.AlgoComboBox.SelectedItem.ToString().EndsWith("Round Robin"))
+            {
+                this.ParamEntryGrid.Visibility = Visibility.Visible;
+                this.ParamTextBlock.Text = "Quantum:";
+            }
+            else if (this.AlgoComboBox.SelectedItem.ToString().EndsWith("Multi Niveaux"))
+            {
+                this.ParamEntryGrid.Visibility = Visibility.Visible;
+                this.ParamTextBlock.Text = "Nombre des Niveaux:";
+            }
+            else if (this.ParamEntryGrid != null)
+            {
+                this.ParamEntryGrid.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void RandomizeCheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.NbProcessusGrid != null)
+            {
+                if (this.NbProcessusGrid.Visibility == Visibility.Visible) this.NbProcessusGrid.Visibility = Visibility.Hidden;
+                else this.NbProcessusGrid.Visibility = Visibility.Visible;
+            }
+            if (this.InputFieldsGrid != null)
+            {
+                if (this.InputFieldsGrid.Visibility == Visibility.Visible) this.InputFieldsGrid.Visibility = Visibility.Hidden;
+                else this.InputFieldsGrid.Visibility = Visibility.Visible;
+            }
+        }
+
+        /*private void EnterButton_Click(object sender, RoutedEventArgs e)
+        {
+            bool random = (bool)this.RandomizeCheckBox.IsChecked;
+            int NbProcessus;
+            bool correct = int.TryParse(this.NbProcessusTextBox.Text, out NbProcessus);
+            if (correct && NbProcessus > 0)
+            {
+                this.MainStackPanel.Children.RemoveRange(0, this.MainStackPanel.Children.Count - 1);  // vider la fenétre
+            }
+            else
+            {
+                if (correct)
+                {
+                    MessageBox.Show("Nombre des processus incorrect");
+                }
+                else
+                {
+                    MessageBox.Show("Input invalid");
+                }
+                this.NbProcessusTextBox.Text = "";
+                return;
+            }/*
+            Random r = new Random();
+            for (int i = 0; i < NbProcessus; i++)
+            {
+                Processus pro = new Processus();
+                pro.id = (i + 1).ToString();
+                pro.duree = (random ? r.Next(1, 10) : 1).ToString();
+                pro.tempsArriv = (random ? r.Next(1, 10) : 0).ToString();
+                pro.Priorite = (random ? r.Next(1, 10) : 1).ToString();
+                this.InitTab.Items.Add(pro);
+            }
+        }*/
     }
 }
