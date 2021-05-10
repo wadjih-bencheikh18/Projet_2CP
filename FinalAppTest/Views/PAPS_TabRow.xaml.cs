d832820d074f902bfa0f3ce262c441bf4ace5dcc
NazimKs <jm_keskes@esi.dz>
2021-05-10 16:10:00 +0100

    test3

---
+++ FinalAppTest/Views/PAPS_TabRow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FinalAppTest.Views
+{
+    /// <summary>
+    /// Interaction logic for PAPS_TabRow.xaml
+    /// </summary>
+    public partial class PAPS_TabRow : UserControl
+    {
+        public PAPS_TabRow()
+        {
+            InitializeComponent();
+        }
+        private TextBox id;
+        private TextBox tempsArriv;
+        private TextBox duree;
+
+        private Grid Table;
+        public PAPS_TabRow(TextBox id,TextBox tempsArriv,TextBox duree,Grid Table)
+        {
+            InitializeComponent();
+            this.id = id;
+            this.tempsArriv = tempsArriv;
+            this.duree = duree;
+            this.Table = Table;
+
+        }
+        private void modifier_Button_Click(object sender, RoutedEventArgs e)
+        {
+            id.Text = idTest.Text;
+            tempsArriv.Text = tempsArrTest.Text;
+            duree.Text = dureeTest.Text;
+            PAPS_Tab.modifier = true;
+            PAPS_Tab.proModifier = this;
+
+        }
+
+        private void suprimer_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Table.Children.Remove(this);
+        }
+    }
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# Projet_2CP
-Outil d'aide à la simulation visuelle des algorithmes d'ordonnancement de processus par un système d’exploitation.
