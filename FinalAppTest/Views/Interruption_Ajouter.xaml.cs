249f720d69f4c53a3c219e48ed7a6964fcddc7ad
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2021-05-28 19:48:55 +0100

    fix PSPDYNAMIQUE

--- FinalAppTest/Views/Interruption_Ajouter.xaml.cs
+++ FinalAppTest/Views/Interruption_Ajouter.xaml.cs
@@ -220,33 +220,6 @@ namespace FinalAppTest.Views
                     ((PSP_TabRow)processus).parent.Items.Add(new Interruption_Ajouter(((PSP_TabRow)processus)));  // append ajouter_row
                 }
             }
-            if (processus.GetType() == typeof(PSP_TabRow))
-            {
-                bool valide = true;
-                var bc = new BrushConverter();
-                if (!Int32.TryParse(tempsArrTest.Text, out int tempsArriv) || tempsArriv <= 0 || tempsArriv >= Int32.Parse(((PSP_TabRow)processus).dureeTest.Text))
-                {
-                    valide = false;
-                    tempsArrTest.Background = (Brush)bc.ConvertFrom("#FFEEBEBE");
-                }
-                if (!Int32.TryParse(dureeTest.Text, out int duree) || duree <= 0)
-                {
-                    valide = false;
-                    dureeTest.Background = (Brush)bc.ConvertFrom("#FFEEBEBE");
-                }
-                if (valide)
-                {
-                    Interruption inter = new Interruption(interType.Text, duree, tempsArriv);
-                    PSP_Tab.prog.listeProcessus.Find(p => p.id == int.Parse(((PSP_TabRow)processus).idTest.Text)).Push(inter);
-                    Interruption_TabRow row = new Interruption_TabRow((PSP_TabRow)processus)
-                    {
-                        DataContext = inter
-                    };
-                    ((PSP_TabRow)processus).parent.Items.RemoveAt(((PSP_TabRow)processus).parent.Items.Count - 1);  // remove the ajouter_row
-                    ((PSP_TabRow)processus).parent.Items.Add(row);
-                    ((PSP_TabRow)processus).parent.Items.Add(new Interruption_Ajouter(((PSP_TabRow)processus)));  // append ajouter_row
-                }
-            }
             if (processus.GetType() == typeof(PRIO_TabRow))
             {
                 bool valide = true;
